066958af1b39ead2734ebb716f334af4ef639cb6
Grillon Thierry <thierry.grillon@gmail.com>
2015-10-29 21:19:49 +0100

    version 0.0.2
    Contient :
    
    genuser.sh : je l'ai amelioré pour des raisons de securite evidentes

--- genuser.sh
+++ genuser.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env ksh
 groupid=1000
 userid=1000
+function hex {
+	openssl rand -hex 8
+}
 function gen {
 userid=$((userid+1))
 /usr/sbin/useradd -u $userid -g $groupid -s /bin/bash -d /home/$user -m $user
-$user:Tempo123 | chpasswd
+passwd=$(hex)
+$user:$passwd | chpasswd
+echo "-- Mdp de $user -> $passwd"
 }
 /usr/sbin/groupadd -g $groupid apprensemble
 for user in steeve jacques laurent patrick abdel eric sebastien stephane herve vincent jeanluc alioune
