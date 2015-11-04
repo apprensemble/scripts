9ed8fec67a8a11edf389518649323cf8b558f5a8
thierry <thierry@apprensemble.link>
2015-11-04 15:11:54 +0000

    ajout de la prise en charge multipartage
    J'ai ajouté le multipartage au niveau du script tmux aePartage.sh

--- aePartage.sh
+++ aePartage.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
-if [ "$1" = "ro" ];then
-  ro=-r
-fi
 socket=/tmp/apprensemble
 session=apprensemble
+while [ ! -z "$*" ]
+do
+if [ "$1" = "ro" ];then
+  ro=-r
+  shift
+elif [[ "$1" = '--session' ]];then
+  session=$2
+  shift
+  shift
+elif [ "$1" = '--socket' ];then
+  socket=/tmp/$2
+  shift
+  shift
+fi
+done
 if [ ! -e $socket ];then
   tmux -S $socket new -s $session
 else
